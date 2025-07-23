b9e76268b1c3b384e1ce56686c875526544bd160
Jose Alfredo Urrea Moreno <jourream@gmail.com>
2025-07-23 10:58:55 -0400

    "4th commit"

--- RiemmanMappingTheorem/Basic.lean
+++ RiemmanMappingTheorem/Basic.lean
@@ -1,1 +1,7 @@
-def hello := "world"
+import Mathlib.Data.Nat.Basic
+
+def my_definition (n : ℕ) := n + 1
+
+theorem my_lemma (n : ℕ) : my_definition n > n := by
+  unfold my_definition
+  exact Nat.lt_add_one n
--- RiemmanMappingTheorem/test.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Data.Nat.Basic
-
-
-def my_definition (n : ℕ) := n + 1
-
-theorem my_lemma (n : ℕ) : my_definition n > n := by
-  unfold my_definition
-  exact Nat.lt_add_one n
